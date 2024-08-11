477c693f3490f3b644d9d723a01a9f41ce551792
Bugra AYAN* <112655530+bugratr@users.noreply.github.com>
2024-08-11 23:39:12 +0300

    Update treasury.py

--- agoric_sdk/agoric_sdk/treasury.py
+++ agoric_sdk/agoric_sdk/treasury.py
@@ -8,5 +8,27 @@ class Treasury:
         else:
             self.funds[source] = amount
 
-    def get_funds(self):
-        return self.funds
+    def get_funds(self, source):
+        return self.funds.get(source, 0)
+
+    def transfer_funds(self, source, destination, amount):
+        if self.funds.get(source, 0) >= amount:
+            self.funds[source] -= amount
+            if destination in self.funds:
+                self.funds[destination] += amount
+            else:
+                self.funds[destination] = amount
+        else:
+            raise ValueError("Insufficient funds in source")
+
+# Usage Example
+treasury = Treasury()
+
+# Fon ekleme
+treasury.add_funds("Government", 1000)
+print("Funds in Government:", treasury.get_funds("Government"))
+
+# Fon transferi
+treasury.transfer_funds("Government", "Education", 200)
+print("Funds in Government after transfer:", treasury.get_funds("Government"))
+print("Funds in Education after transfer:", treasury.get_funds("Education"))
